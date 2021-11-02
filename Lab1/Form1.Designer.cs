aa271704b6912bf7f34779666aff014f35998e12
volcha <57828516+volcha@users.noreply.github.com>
2021-11-02 14:24:50 +0300

    Add files via upload

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Функция";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(776, 450);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(131, 473);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 476);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "a =";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 503);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(327, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Построить график функции y = ax^(3/2), 0 <= x <= B";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(414, 476);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "B =";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(443, 473);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 538);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная № 1";
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        List<double> Xdots;
+        List<double> Ydots;
+        //Строим кривую
+        private void button1_Click(object sender, EventArgs e) {
+            chart1.Series[0].Points.Clear();
+            string sa = textBox1.Text.Replace('.', ',');
+            string sB = textBox2.Text.Replace('.', ',');
+            double a = Convert.ToDouble(sa);
+            double B = Convert.ToDouble(sB);
+            double step = 0.01;
+            Xdots = new List<double>();
+            Ydots = new List<double>();
+            for (double x = 0; x <= B; x += step) {
+                double y = a * Math.Pow(x, 1.5);
+                Xdots.Add(x);
+                Ydots.Add(y);
+            }
+            for (int i = 0; i < Xdots.Count; i++) {
+                chart1.Series[0].Points.AddXY(Xdots[i], Ydots[i]);
+            }
+        }
+        //Обеспечиваем автоматическое масштабирование при изменении размеров окна
+        private void Form1_ResizeEnd(object sender, EventArgs e) {
+            chart1.Series[0].Points.Clear();
+            chart1.Size = new Size(ClientSize.Width - 24, ClientSize.Height - 89);
+            label1.Location = new Point(103, chart1.Size.Height + 27);
+            textBox1.Location = new Point(131, chart1.Size.Height + 24);
+            label2.Location = new Point(103 + chart1.Size.Width / 2, chart1.Size.Height + 27);
+            textBox2.Location = new Point(131 + chart1.Size.Width / 2, chart1.Size.Height + 24);
+            button1.Location = new Point(chart1.Size.Width / 2 - 152, chart1.Size.Height + 55);
+            if (Xdots != null) {
+                for (int i = 0; i < Xdots.Count; i++) {
+                    chart1.Series[0].Points.AddXY(Xdots[i], Ydots[i]);
+                }
+            }
+        }
+    }
+}
